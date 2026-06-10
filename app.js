7084390ac09612dd4a1c8caa417e06ce91e86107
sischramm <schramm.adm@gmail.com>
2026-06-10 20:27:12 -0300

    Update app.js

--- app.js
+++ app.js
@@ -412,8 +412,44 @@ window.carregarFiliais = carregarFiliais;
 
 async function importarJogos(){
 
-  alert(
-    "CSV recebido. Próximo passo: gerar importação completa dos 72 jogos."
-  );
+  const jogos = [
+
+    {
+      id:1,
+      fase:"Grupos",
+      grupo:"A",
+      timeA:"México",
+      timeB:"África do Sul",
+      dataHora:"2026-06-11 16:00:00"
+    },
+
+    {
+      id:2,
+      fase:"Grupos",
+      grupo:"A",
+      timeA:"Coreia do Sul",
+      timeB:"República Tcheca",
+      dataHora:"2026-06-11 23:00:00"
+    }
+
+  ];
+
+  for(const jogo of jogos){
+
+    await addDoc(
+      collection(db,"jogos"),
+      {
+        ...jogo,
+        encerrado:false,
+        placarRealA:null,
+        placarRealB:null
+      }
+    );
+
+  }
+
+  alert("Jogos importados!");
 
 }
+
+window.importarJogos = importarJogos;
