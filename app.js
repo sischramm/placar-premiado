ff2abe444c067bdc7aa8aa1da155793ef5ed1c85
sischramm <schramm.adm@gmail.com>
2026-06-10 19:27:58 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,38 @@
+function abrirAba(nome){
+  alert("Abrir aba: " + nome);
+}
+
+function sair(){
+  localStorage.clear();
+  location.reload();
+}
+
+function toggleSenha(id, botao){
+
+  const campo =
+  document.getElementById(id);
+
+  if(campo.type === "password"){
+    campo.type = "text";
+    botao.innerHTML = "🙈";
+  }else{
+    campo.type = "password";
+    botao.innerHTML = "👁";
+  }
+}
+
+function login(){
+  alert("Login será conectado ao Firebase");
+}
+
+function cadastrar(){
+  alert("Cadastro será conectado ao Firebase");
+}
+
+function salvarExtras(){
+  alert("Extras serão conectados ao Firebase");
+}
+
+function recalcularRanking(){
+  alert("Admin");
+}
