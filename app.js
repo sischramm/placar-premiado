990ec56695a978033e03b611d4c17148e4e4f21f
sischramm <schramm.adm@gmail.com>
2026-06-10 19:35:14 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
+window.onload = function() {
+
+  const home = document.getElementById("homeInicial");
+  const login = document.getElementById("loginSistema");
+  const topo = document.getElementById("topoSistema");
+  const sistema = document.getElementById("sistemaCompleto");
+
+  if(home) home.style.display = "flex";
+  if(login) login.style.display = "none";
+  if(topo) topo.style.display = "none";
+  if(sistema) sistema.style.display = "none";
+
+};
+
 function abrirAba(nome){
   alert("Abrir aba: " + nome);
 }
@@ -36,6 +50,7 @@ function salvarExtras(){
 function recalcularRanking(){
   alert("Admin");
 }
+
 function abrirLogin() {
 
   document.getElementById("homeInicial").style.display = "none";
@@ -50,6 +65,8 @@ function abrirCadastro() {
 
   document.getElementById("loginSistema").style.display = "grid";
 
-  document.getElementById("nome").focus();
+  setTimeout(() => {
+    document.getElementById("nome").focus();
+  }, 100);
 
 }
