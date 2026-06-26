2a518dbc10af004c4419e0001030365dcb4c5c83
sischramm <schramm.adm@gmail.com>
2026-06-26 00:07:23 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,86 +3,79 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/fireba
 import {
 
   getFirestore,
-
   collection,
-
+  doc,
+  getDoc,
+  getDocs,
+  setDoc,
   addDoc,
-
-  getDocs
+  serverTimestamp
 
 } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
 
 
-// ==============================
+// ==============================================
 // FIREBASE PRINCIPAL
-// ==============================
+// ==============================================
 
 const firebasePrincipal = {
 
   apiKey: "AIzaSyC4PWHWsENLuUPnBVIzo8U8nkDXVA2fTqI",
-
   authDomain: "novo-placar-premiado.firebaseapp.com",
-
   projectId: "novo-placar-premiado",
-
   storageBucket: "novo-placar-premiado.firebasestorage.app",
-
   messagingSenderId: "597865499198",
-
   appId: "1:597865499198:web:fe2274064f70f681bd5f6f"
 
 };
 
 
-// ==============================
+// ==============================================
 // FIREBASE MATA-MATA
-// ==============================
+// ==============================================
 
 const firebaseMata = {
 
   apiKey: "AIzaSyC92ucYcknsMjYknvXXVRxiVkHu2SmJrCU",
-
   authDomain: "placar-premiado-mata.firebaseapp.com",
-
   projectId: "placar-premiado-mata",
-
   storageBucket: "placar-premiado-mata.firebasestorage.app",
-
   messagingSenderId: "834430772323",
-
   appId: "1:834430772323:web:be92b48f7dd951f718c456"
 
 };
 
 
-// Inicializa os dois projetos
+// Apps
 
 const appPrincipal = initializeApp(firebasePrincipal);
 
 const appMata = initializeApp(firebaseMata,"mata");
 
 
-// Bancos
+// Databases
 
-export const db = getFirestore(appPrincipal);
+const db = getFirestore(appPrincipal);
 
-export const dbMata = getFirestore(appMata);
+const dbMata = getFirestore(appMata);
 
 
-// Principal
+// Window
 
 window.db = db;
 
-
-// Mata-Mata
-
 window.dbMata = dbMata;
-
-
-// Funções
 
 window.collection = collection;
 
-window.addDoc = addDoc;
+window.doc = doc;
+
+window.getDoc = getDoc;
 
 window.getDocs = getDocs;
+
+window.setDoc = setDoc;
+
+window.addDoc = addDoc;
+
+window.serverTimestamp = serverTimestamp;
