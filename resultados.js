5b7d12190bdaa2b4bde63b6dbdb24d31217369cc
sischramm <schramm.adm@gmail.com>
2026-06-30 16:20:12 -0300

    Update resultados.js

--- resultados.js
+++ resultados.js
@@ -3,3 +3,15 @@
 // Grupo Shark - Copa 2026
 // Versão 2
 // =======================================
+
+function imagemBandeira(sigla){
+
+    if(!window.FLAGS) return "";
+
+    const codigo = window.FLAGS[sigla];
+
+    if(!codigo) return "";
+
+    return `https://flagcdn.com/w80/${codigo}.png`;
+
+}
